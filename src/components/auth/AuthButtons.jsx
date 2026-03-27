17dd2773433b0216f699bb8d8b900e3c8636214c
Niaal-B <nihnihal504@gmail.com>
2026-03-27 10:54:40 +0530

    fix(auth): remove sensitive calendar scope from Google OAuth to bypass verification

--- src/components/auth/AuthButtons.jsx
+++ src/components/auth/AuthButtons.jsx
@@ -6,7 +6,6 @@ const GoogleAuthButtons = () => {
     const GOOGLE_SCOPES = [
         'https://www.googleapis.com/auth/userinfo.profile',
         'https://www.googleapis.com/auth/userinfo.email',
-        'https://www.googleapis.com/auth/calendar',
         'openid'
     ].join(' ');
 
